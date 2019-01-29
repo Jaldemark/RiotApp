5c7a0f566ed208a992da57c8e5193d5f88e4ca69
Jaldemark <joeljaldemark@outlook.com>
2019-01-29 22:33:12 +0100

    kda

--- getwin.py
+++ getwin.py
@@ -3,6 +3,12 @@ from championId import ChNameToId
 from seasons import seasonId
 from api import apikey
 from queueId import queueId
+from kda import getkda
+def getParId(account,match):
+    for key in match.json()['participantIdentities']:
+        if key['player']['summonerName']==account:
+            return key['participantId']
+    return 11
 def getwin(account, champ):
 
     URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+account+"?api_key="+apikey()
@@ -11,30 +17,38 @@ def getwin(account, champ):
     #AccountId visar error ibland av oklar anledning
     URL2 = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/"+rj['accountId']+"?champion="+str(ChNameToId(champ))+"&api_key="+apikey()
     match2 = requests.get(URL2)
-    print(match2.json())
     wincounter =0
     fittcounters =0
     count = 0
-    fitterror = 0
-    while(count<match2.json()['endIndex']):
-        #try:
+    kda =[0,0,0]
+    index = match2.json()['endIndex']
+    while(count<(match2.json()['endIndex']-1)):
+        try:
             gameId = match2.json()['matches'][count]['gameId']
             season = match2.json()['matches'][count]['season']
             queuetype = match2.json()['matches'][count]['queue']
-            URL =  "https://euw1.api.riotgames.com/lol/match/v3/matches/"+str(gameId)+"?api_key="+apikey()
+
+            URL =  "https://euw1.api.riotgames.com/lol/match/v4/matches/"+str(gameId)+"?api_key="+apikey()
             match = requests.get(URL)
-            if queuetype == 420:
-                print(match.json())
-                if match.json()['teams'][0]['win'] == 'Win':
+
+            #if queuetype == 420:
+
+            if getParId(account,match) != 11:
+                noob = getkda(match.json()['participants'][getParId(account,match)-1]['stats'])
+                lists_of_lists = [kda, getkda(match.json()['participants'][getParId(account,match)-1]['stats'])]
+                kda = [sum(x) for x in zip(*lists_of_lists)]
+                if getParId(account,match) != 11 and match.json()['participants'][getParId(account,match)-1]['stats']['win'] == True:
                     wincounter = wincounter + 1
                 fittcounters = fittcounters+1
-                print('Game: ',fittcounters,'Win counter: ',wincounter, seasonId(season), 'queue: ', queueId(queuetype))
-            count = count + 1
-            #print('Game: ',count,'Win counter: ',wincounter, seasonId(season), 'queue: ', queueId(queuetype))
 
-        #except:
-              # print('Fitt error')
-              # count = count+1
-               #fitterror = fitterror+1
+                noobstr = 'K/D/A: '+str(kda[0])+'/'+str(kda[1])+'/'+str(kda[2])
+                print('Game: ',fittcounters,'Win counter: ',wincounter, seasonId(season), 'queue: ', queueId(queuetype), noob)
+                count = count + 1
+        except Exception as e:
+               print(e)
+               count = count+1
 
-    return wincounter/fittcounters
+    kda[0]=kda[0]/index
+    kda[1]=kda[1]/index
+    kda[2]=kda[2]/index
+    return [wincounter/fittcounters, kda]
--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import requests
 import json
 from championId import ChNameToId
-apikey = "RGAPI-03ded2e1-f816-48cb-8a05-31e858cd703f"
+from api import apikey
 
-
-
-
-URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Jaldemark?api_key="+apikey
+URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Jaldemark?api_key="+apikey()
 response = requests.get(URL)
 rj = response.json()
 
-URL2 = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/"+rj['accountId']+"?champion=13&api_key="+apikey
+URL2 = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/"+rj['accountId']+"?champion=13&api_key="+apikey()
 match2 = requests.get(URL2)
+count = 0
+while(count<match2.json()['endIndex']):
 
-print(match2.json())
+    game = match2.json()['matches'][count]['gameId']
+    URL3 =  "https://euw1.api.riotgames.com/lol/match/v4/matches/"+str(game)+"?api_key="+apikey()
+    response = requests.get(URL3)
+    match = response.json()
+    print(match['participants'][count]['stats'])
+    print('-------------------------------------------------------------------------------------')
+    count = count+1
